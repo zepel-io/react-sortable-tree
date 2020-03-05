1379fd3d2a6ffd33c163398df416ee3a9ef5a60e
Subramaniam <bsubramaniam91@gmail.com>
2020-03-05 15:28:42 +0530

    Built dist

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2375,6 +2375,7 @@ function () {
         var _monitor$getItem = monitor.getItem(),
             node = _monitor$getItem.node;
 
+        console.log(node, targetDepth, dropTargetProps);
         var addedResult = memoizedInsertNode({
           treeData: this.treeData,
           newNode: node,
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2367,6 +2367,7 @@ function () {
         var _monitor$getItem = monitor.getItem(),
             node = _monitor$getItem.node;
 
+        console.log(node, targetDepth, dropTargetProps);
         var addedResult = memoizedInsertNode({
           treeData: this.treeData,
           newNode: node,
